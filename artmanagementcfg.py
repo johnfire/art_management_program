1b486330693c96a4b66859217a22db5ebe27a201
christopher rehm <christopherrehm@web.de>
2020-01-06 20:53:47 +0100

    config and sset up files

---
+++ artmanagementcfg.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 21 11:28:41 2019
+
+@author: christopher
+"""
+
+basicPathDir = "/home/christopher/Pictures/myPaintings"
+workingDir = "" # this is the path to the active directory
+
+subDirList = []
+subDirPaths = []
+paintingList = []
+paintingPaths= []
+paintingDic = {}
+myData ={}
+
+myworkingFolder ="" #the name of the active painting directory
+answer = "2"  # 1 is the terminal version 2 is the gui version
+
+dirlist = {"displate","mainphoto","society6","xcftype","pngtype","jpgtype","cr2type","info","oldcanon","oldCameraPics"}
