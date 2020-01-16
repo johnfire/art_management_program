d82592539d7f3a12cad035491f159df2f8277b63
christopher rehm <christopherrehm@web.de>
2020-01-16 20:17:34 +0100

    some organizational changes to file

--- artmanagementcfg.py
+++ artmanagementcfg.py
@@ -5,9 +5,14 @@ Created on Sat Sep 21 11:28:41 2019
 
 @author: christopher
 """
+dirlist = {"displate","mainphoto","society6","xcftype","pngtype","jpgtype","cr2type","info","oldcanon","oldCameraPics"}
+galleryList = ["mysite","buzzart","deviant","saatchi","soc6"]
 
 basicPathDir = "/home/christopher/Pictures/myPaintings"
-workingDir = "" # this is the path to the active directory
+workingDir = '/home/christopher/Pictures/myPaintings/finishedWorks-forSale7:30-09-09-19' # this is the path to the active directory
+
+#DO NOT CHANGE ANYTHING BELOW HERE FOR YOUR SET UP
+
 myworkingFolder = "" #the name of the active painting directorysubDirList = []
 
 subDirPaths = []
@@ -23,6 +28,12 @@ mylibOfSubdirectories = {}
 dirOfCatandPaintings = {}
 mylibOfSubPaintings = {}
 
+gallery1 = galleryList[0]
+gallery2 = galleryList[1]
+gallery3 = galleryList[2]
+gallery4 = galleryList[3]
+gallery5 = galleryList[4]
+
 listSubs = []
 totalSubs = 0
 catIndex = 1
@@ -32,18 +43,8 @@ totalPtngs = 0
 ptngIndex = 0
 dispPainting = ""
 
-
-dirlist = {"displate","mainphoto","society6","xcftype","pngtype","jpgtype","cr2type","info","oldcanon","oldCameraPics"}
-
-gallery1 = "mysite"
-gallery2 = "buzzart"
-gallery3 = "deviant"
-gallery4 = "saatchi"
-gallery5 = "soc6"
-
 nameOfPaintingfile = "none"
 nameOfCatagory = "none"
-
 nameOfPainting = "none"
 datePainted = "none"
 wherePainted = "none"
