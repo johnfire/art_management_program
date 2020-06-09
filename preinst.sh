b9911bb20b3785fca0e6737e6244a0ccf8f011d5
Christopher Rehm <christopherrehm@web.de>
2020-06-09 14:34:33 +0200

    preinstall script for art management program

---
+++ preinst.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt-get install python3-pip -y
+
+pip3 install markdown
+pip3 install wkhtmltopdf
+pip3 install pillow
+pip3 install pdfkit
+pip3 install -U -f https://extras.wxpython.org/wxPython4/extras/linux/gtk3/ubuntu-18.04 wxPython
